92e1746f02bb568ef4ee1d83a4fee5257d71a959
joao1barbosa <joao1.barbosa@outlook.com>
2025-01-12 12:09:38 -0300

    feat: add new tabs for user navigation

---
+++ src/app/(shop)/orders/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text, StyleSheet } from "react-native"
+
+const Orders = () => {
+    return (
+        <View>
+            <Text>Orders</Text>
+        </View>
+    )
+};
+
+export default Orders;
+
+const styles = StyleSheet.create({});
